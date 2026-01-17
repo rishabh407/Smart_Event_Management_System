79d30b36381885f8d4dc9918bb3ace9d5d7f810b
Rohit <rishmehr10414@gmail.com>
2026-01-18 01:58:11 +0530

    Uploaded Team Competition templates successfully.

--- Backend/src/controllers/certificate.controller.js
+++ Backend/src/controllers/certificate.controller.js
@@ -1,14 +1,160 @@
+// import { v4 as uuidv4 } from "uuid";
+// import Certificate from "../models/Certificate.js";
+// import CertificateTemplate from "../models/CertificateTemplate.js";
+// import Registration from "../models/Registration.js";
+// import Result from "../models/Result.js";
+// import User from "../models/User.js";
+// import { generateCertificatePDF } from "../utils/certificateGenerator.js";
+// import Competition from "../models/Competition.js";
+// import Team from "../models/Team.js";
+
+// export const generateCertificates = async (req, res) => {
+//   const { competitionId } = req.body;
+
+//   if (!competitionId) {
+//     return res.status(400).json({
+//       message: "competitionId required"
+//     });
+//   }
+
+//   const participationTemplate = await CertificateTemplate.findOne({
+//     competitionId,
+//     type: "participation"
+//   });
+
+//   const winnerTemplate = await CertificateTemplate.findOne({
+//     competitionId,
+//     type: "winner"
+//   });
+
+//   if (!participationTemplate || !winnerTemplate) {
+//     return res.status(400).json({
+//       message: "Both templates required"
+//     });
+//   }
+
+//   const registrations = await Registration.find({
+//     competitionId,
+//     status: "attended",
+//     certificateGenerated: false
+//   });
+
+//   if (registrations.length === 0) {
+//     return res.json({
+//       message: "No new certificates to generate"
+//     });
+//   }
+
+//   const winners = await Result.find({ competitionId });
+
+//   const competition = await Competition.findOne({ competitionId });
+
+//   if (!competition) {
+//     return res.status(404).json({
+//       message: "Competition not found"
+//     });
+//   }
+
+//   const generatedCertificates = [];
+
+//   for (const reg of registrations) {
+
+//     // const winner = winners.find(
+//     //   w => w.participantId === reg.studentId
+//     // );
+
+//     const winner = winners.find(w => {
+
+//   // Individual winner
+//   if (w.type === "student") {
+//     return w.participantId === reg.studentId;
+//   }
+
+//   // Team winner
+//   if (w.type === "team") {
+//     return w.participantId === reg.teamId;
+//   }
+
+// });
+
+//     const isWinner = !!winner;
+
+//     const template = isWinner
+//       ? winnerTemplate
+//       : participationTemplate;
+
+//     // const user = await User.findOne({
+//     //   userId: reg.studentId
+//     // });
+
+//      let displayName;
+
+// if (reg.studentId) {
+
+//   const user = await User.findOne({ userId: reg.studentId });
+//   displayName = user.fullName;
+
+// } else if (reg.teamId) {
+
+//   const team = await Team.findOne({ teamId: reg.teamId });
+//   displayName = team.teamName;
+// }
+
+    
+//     const pdfPath = await generateCertificatePDF({
+//       name: user.fullName,
+//       competitionName: competition.name,
+//       position: isWinner ? winner.position : null,
+//       templatePath: template.templatePath,
+//       textConfig: template.textConfig
+//     });
+
+//     const cert = await Certificate.create({
+//       certificateId: uuidv4(),
+//       competitionId,
+//       userId: reg.studentId,
+//       type: isWinner ? "winner" : "participation",
+//       position: isWinner ? winner.position : null,
+//       pdfUrl: pdfPath
+//     });
+
+//     // 🔥 IMPORTANT FLAG
+//     reg.certificateGenerated = true;
+//     await reg.save();
+
+//     generatedCertificates.push(cert);
+//   }
+
+//   res.json({
+//     message: "Certificates generated successfully",
+//     generatedCount: generatedCertificates.length
+//   });
+// };
+
+// export const getMyCertificates = async (req, res) => {
+
+//   const certificates = await Certificate.find({
+//     userId: req.user.userId
+//   });
+
+//   res.json(certificates);
+// };
+
 import { v4 as uuidv4 } from "uuid";
 import Certificate from "../models/Certificate.js";
 import CertificateTemplate from "../models/CertificateTemplate.js";
 import Registration from "../models/Registration.js";
 import Result from "../models/Result.js";
 import User from "../models/User.js";
-import { generateCertificatePDF } from "../utils/certificateGenerator.js";
-import Competition from "../models/Competition.js";
 import Team from "../models/Team.js";
+import Competition from "../models/Competition.js";
+import { generateCertificatePDF } from "../utils/certificateGenerator.js";
+
+
+// ================= GENERATE CERTIFICATES =================
 
 export const generateCertificates = async (req, res) => {
+
   const { competitionId } = req.body;
 
   if (!competitionId) {
@@ -17,21 +163,48 @@ export const generateCertificates = async (req, res) => {
     });
   }
 
+  // ---------------- FETCH COMPETITION ----------------
+
+  const competition = await Competition.findOne({ competitionId });
+
+  if (!competition) {
+    return res.status(404).json({
+      message: "Competition not found"
+    });
+  }
+
+  // ---------------- AUTO TEMPLATE TYPE ----------------
+
+  let participationType;
+  let winnerType;
+
+  if (competition.eventType === "team") {
+    participationType = "participant_hackathon";
+    winnerType = "winner_hackathon";
+  } else {
+    participationType = "participation";
+    winnerType = "winner";
+  }
+
+  // ---------------- FETCH TEMPLATES ----------------
+
   const participationTemplate = await CertificateTemplate.findOne({
     competitionId,
-    type: "participation"
+    type: participationType
   });
 
   const winnerTemplate = await CertificateTemplate.findOne({
     competitionId,
-    type: "winner"
+    type: winnerType
   });
 
   if (!participationTemplate || !winnerTemplate) {
     return res.status(400).json({
-      message: "Both templates required"
+      message: "Required certificate templates not uploaded"
     });
   }
+
+  // ---------------- FETCH ELIGIBLE REGISTRATIONS ----------------
 
   const registrations = await Registration.find({
     competitionId,
@@ -45,37 +218,29 @@ export const generateCertificates = async (req, res) => {
     });
   }
 
+  // ---------------- FETCH WINNERS ----------------
+
   const winners = await Result.find({ competitionId });
 
-  const competition = await Competition.findOne({ competitionId });
+  let generatedCount = 0;
 
-  if (!competition) {
-    return res.status(404).json({
-      message: "Competition not found"
-    });
-  }
-
-  const generatedCertificates = [];
+  // ---------------- MAIN LOOP ----------------
 
   for (const reg of registrations) {
 
-    // const winner = winners.find(
-    //   w => w.participantId === reg.studentId
-    // );
+    // ---------- Check Winner ----------
 
     const winner = winners.find(w => {
 
-  // Individual winner
-  if (w.type === "student") {
-    return w.participantId === reg.studentId;
-  }
+      if (w.type === "student") {
+        return w.participantId === reg.studentId;
+      }
 
-  // Team winner
-  if (w.type === "team") {
-    return w.participantId === reg.teamId;
-  }
+      if (w.type === "team") {
+        return w.participantId === reg.teamId;
+      }
 
-});
+    });
 
     const isWinner = !!winner;
 
@@ -83,53 +248,87 @@ export const generateCertificates = async (req, res) => {
       ? winnerTemplate
       : participationTemplate;
 
-    // const user = await User.findOne({
-    //   userId: reg.studentId
-    // });
+    // ================= TEAM COMPETITION =================
 
-     let displayName;
+    if (competition.type === "team" && reg.teamId) {
 
-if (reg.studentId) {
+      const team = await Team.findOne({ teamId: reg.teamId });
 
-  const user = await User.findOne({ userId: reg.studentId });
-  displayName = user.fullName;
+      if (!team) continue;
 
-} else if (reg.teamId) {
+      for (const memberId of team.members) {
 
-  const team = await Team.findOne({ teamId: reg.teamId });
-  displayName = team.teamName;
-}
+        const user = await User.findOne({ userId: memberId });
 
-    
-    const pdfPath = await generateCertificatePDF({
-      name: user.fullName,
-      competitionName: competition.name,
-      position: isWinner ? winner.position : null,
-      templatePath: template.templatePath,
-      textConfig: template.textConfig
-    });
+        if (!user) continue;
 
-    const cert = await Certificate.create({
-      certificateId: uuidv4(),
-      competitionId,
-      userId: reg.studentId,
-      type: isWinner ? "winner" : "participation",
-      position: isWinner ? winner.position : null,
-      pdfUrl: pdfPath
-    });
+        const pdfPath = await generateCertificatePDF({
+          name: user.fullName,
+          teamName: team.teamName,
+          competitionName: competition.name,
+          position: isWinner ? winner.position : null,
+          templatePath: template.templatePath,
+          textConfig: template.textConfig
+        });
 
-    // 🔥 IMPORTANT FLAG
+        await Certificate.create({
+          certificateId: uuidv4(),
+          competitionId,
+          userId: memberId,
+          teamId: team.teamId,
+          type: isWinner ? "winner" : "participation",
+          position: isWinner ? winner.position : null,
+          pdfUrl: pdfPath
+        });
+
+        generatedCount++;
+      }
+
+    }
+
+    // ================= INDIVIDUAL COMPETITION =================
+
+    else {
+
+      const user = await User.findOne({ userId: reg.studentId });
+
+      if (!user) continue;
+
+      const pdfPath = await generateCertificatePDF({
+        name: user.fullName,
+        competitionName: competition.name,
+        position: isWinner ? winner.position : null,
+        templatePath: template.templatePath,
+        textConfig: template.textConfig
+      });
+
+      await Certificate.create({
+        certificateId: uuidv4(),
+        competitionId,
+        userId: reg.studentId,
+        type: isWinner ? "winner" : "participation",
+        position: isWinner ? winner.position : null,
+        pdfUrl: pdfPath
+      });
+
+      generatedCount++;
+    }
+
+    // ---------- Mark Registration Done ----------
+
     reg.certificateGenerated = true;
     await reg.save();
-
-    generatedCertificates.push(cert);
   }
 
   res.json({
     message: "Certificates generated successfully",
-    generatedCount: generatedCertificates.length
+    generatedCount
   });
+
 };
+
+
+// ================= STUDENT VIEW CERTIFICATES =================
 
 export const getMyCertificates = async (req, res) => {
 
@@ -139,3 +338,4 @@ export const getMyCertificates = async (req, res) => {
 
   res.json(certificates);
 };
+
--- Backend/src/controllers/result.controller.js
+++ Backend/src/controllers/result.controller.js
@@ -1,11 +1,54 @@
+// import { v4 as uuidv4 } from "uuid";
+// import Result from "../models/Result.js";
+// import Competition from "../models/Competition.js";
+
+// /**
+//  * DECLARE RESULTS
+//  */
+// export const declareResults = async (req, res) => {
+//   const { competitionId, winners } = req.body;
+
+//   if (!competitionId || !winners || winners.length === 0) {
+//     return res.status(400).json({
+//       message: "Invalid result data"
+//     });
+//   }
+
+//   const competition = await Competition.findOne({ competitionId });
+
+//   if (!competition) {
+//     return res.status(404).json({
+//       message: "Competition not found"
+//     });
+//   }
+
+//   const savedResults = [];
+
+//   for (const winner of winners) {
+//     const result = await Result.create({
+//       resultId: uuidv4(),
+//       competitionId,
+//       participantId: winner.participantId,
+//       type: winner.type,
+//       position: winner.position
+//     });
+
+//     savedResults.push(result);
+//   }
+
+//   res.status(201).json({
+//     message: "Results declared successfully",
+//     results: savedResults
+//   });
+// };
+
+
 import { v4 as uuidv4 } from "uuid";
 import Result from "../models/Result.js";
 import Competition from "../models/Competition.js";
 
-/**
- * DECLARE RESULTS
- */
 export const declareResults = async (req, res) => {
+
   const { competitionId, winners } = req.body;
 
   if (!competitionId || !winners || winners.length === 0) {
@@ -14,6 +57,7 @@ export const declareResults = async (req, res) => {
     });
   }
 
+  // Fetch competition
   const competition = await Competition.findOne({ competitionId });
 
   if (!competition) {
@@ -22,14 +66,27 @@ export const declareResults = async (req, res) => {
     });
   }
 
+  // Auto decide result type
+  const resultType =
+    competition.type === "team"
+      ? "team"
+      : "student";
+
   const savedResults = [];
 
   for (const winner of winners) {
+
+    if (!winner.participantId || !winner.position) {
+      return res.status(400).json({
+        message: "ParticipantId and position required"
+      });
+    }
+
     const result = await Result.create({
       resultId: uuidv4(),
       competitionId,
       participantId: winner.participantId,
-      type: winner.type,
+      type: resultType,
       position: winner.position
     });
 
